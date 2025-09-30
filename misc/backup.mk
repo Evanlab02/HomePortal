3ba7991a3ebdeccc0dbc352e82b61dbe47ff352b
Evanlab02 <evanlabuschagne70@gmail.com>
2025-09-30 15:09:56 +0200

    chore: Prep for postgres 17 migration

--- misc/backup.mk
+++ misc/backup.mk
@@ -3,3 +3,15 @@ backup-pg15:
 	@echo "Backing up all databases..."
 	@docker compose exec -T postgres-15 pg_dumpall -U postgres > apps/files/data/backups/db/postgres/15/backup_$(shell date +%Y%m%d_%H%M%S).sql
 	@echo "Backup completed successfully"
+
+.PHONY: backup-pg16
+backup-pg16:
+	@echo "Backing up all databases..."
+	@docker compose exec -T postgres-16 pg_dumpall -U postgres > apps/files/data/backups/db/postgres/16/backup_$(shell date +%Y%m%d_%H%M%S).sql
+	@echo "Backup completed successfully"
+
+.PHONY: backup-pg17
+backup-pg17:
+	@echo "Backing up all databases..."
+	@docker compose exec -T postgres-17 pg_dumpall -U postgres > apps/files/data/backups/db/postgres/17/backup_$(shell date +%Y%m%d_%H%M%S).sql
+	@echo "Backup completed successfully"
--- misc/env.mk
+++ misc/env.mk
@@ -5,3 +5,11 @@ migrate-env-pg15-pg16:
 	@sed -i apps/authentik/.env -e 's/AUTHENTIK_POSTGRESQL__HOST=hp-postgres-15/AUTHENTIK_POSTGRESQL__HOST=hp-postgres-16/'
 	@sed -i apps/shopping/.env -e 's/SHOPPING_DB_HOST=hp-postgres-15/SHOPPING_DB_HOST=hp-postgres-16/'
 	@echo "Environment variables migrated successfully"
+
+.PHONY: migrate-env-pg16-pg17
+migrate-env-pg16-pg17:
+	@echo "Migrating environment variables for apps using PostgreSQL 16 to PostgreSQL 17..."
+	@sed -i apps/solidtime/env/app.env -e 's/DB_HOST="hp-postgres-16"/DB_HOST="hp-postgres-17"/'
+	@sed -i apps/authentik/.env -e 's/AUTHENTIK_POSTGRESQL__HOST=hp-postgres-16/AUTHENTIK_POSTGRESQL__HOST=hp-postgres-17/'
+	@sed -i apps/shopping/.env -e 's/SHOPPING_DB_HOST=hp-postgres-16/SHOPPING_DB_HOST=hp-postgres-17/'
+	@echo "Environment variables migrated successfully"
