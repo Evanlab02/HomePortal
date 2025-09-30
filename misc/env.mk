39e0b8c4e4eda58a89b7d86618fed237773608ee
Evanlab02 <evanlabuschagne70@gmail.com>
2025-09-30 14:57:39 +0200

    chore: Create .env migration for postgres

---
+++ misc/env.mk
@@ -0,0 +1,7 @@
+.PHONY: migrate-env-pg15-pg16
+migrate-env-pg15-pg16:
+	@echo "Migrating environment variables for apps using PostgreSQL 15 to PostgreSQL 16..."
+	@sed -i apps/solidtime/env/app.env -e 's/DB_HOST=hp-postgres-15/DB_HOST=hp-postgres-16/'
+	@sed -i apps/authentik/.env-e 's/AUTHENTIK_POSTGRESQL__HOST=hp-postgres-15/AUTHENTIK_POSTGRESQL__HOST=hp-postgres-16/'
+	@sed -i apps/shopping/.env -e 's/SHOPPING_DB_HOST=hp-postgres-15/SHOPPING_DB_HOST=hp-postgres-16/'
+	@echo "Environment variables migrated successfully"
