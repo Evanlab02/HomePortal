bdeb9975b9b3f7f5a8fc1b2a655dc202c38e6371
Evanlab02 <evanlabuschagne70@gmail.com>
2025-09-30 05:13:48 +0200

    chore: Reboot fixes

--- misc/maintenance.mk
+++ misc/maintenance.mk
@@ -3,11 +3,11 @@ reboot:
 	@echo "Sending reboot notification..."
 	@docker compose exec ntfy ntfy pub \
 		-u $(NTFY_USER):$(NTFY_PW) \
-		-t "🔄 Surprise! Time for another reboot!" \
-		-p urgent \
+		--title "🔄 Surprise! Time for another reboot!" \
+		--priority urgent \
 		--tags computer,dizzy_face,warning\
 		--click https://giphy.com/gifs/have-you-tried-turning-it-off-and-on-again-F7yLXA5fJ5sLC \
-		"The server has decided it needs a little nap 💤 Don't worry, it'll be back faster than you can say 'have you tried turning it off and on again?'" \
+		--message "The server has decided it needs a little nap 💤 Don't worry, it'll be back faster than you can say 'have you tried turning it off and on again?'" \
 		https://ntfy.labuschagne.xyz/hp-all
 	@echo "Sent reboot notification"
 	@echo "Scheduling reboot"
