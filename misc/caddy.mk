64ca7ec3f2907de299e7a9c41a67db9e4a6beb14
Evanlab02 <evanlabuschagne70@gmail.com>
2025-09-30 06:21:10 +0200

    chore: More fixes

--- misc/caddy.mk
+++ misc/caddy.mk
@@ -1,3 +1,3 @@
 .PHONY: caddy-fmt
 caddy-fmt:
-	docker run --rm -v `pwd`/apps/web/Caddyfile:/etc/caddy/Caddyfile caddy:2.10.2-alpine caddy fmt /etc/caddy/Caddyfile --overwrite
+	docker run --rm -v `pwd`/apps/web/conf/Caddyfile:/etc/caddy/Caddyfile caddy:2.10.2-alpine caddy fmt /etc/caddy/Caddyfile --overwrite
