929492ad9f64600b8b47931bf7211fd8f49baaaf
Evanlab02 <evanlabuschagne70@gmail.com>
2025-09-30 14:44:27 +0200

    chore: Update backup and migrate commands

--- misc/migrate.mk
+++ misc/migrate.mk
@@ -6,6 +6,7 @@ prep-pg15-pg16:
 
 .PHONY: migrate-pg15-pg16
 migrate-pg15-pg16:
-	@echo "Migrating PostgreSQL 16..."
-	@docker compose exec -T postgres-16 pg_restore -U postgres -d postgres -1 apps/files/data/backups/db/postgres/15/${ARGS}
+	@echo "Migrating all databases to PostgreSQL 16..."
+	@docker compose cp apps/files/data/backups/db/postgres/15/${ARGS} postgres-16:/tmp/backup.sql
+	@docker compose exec -T postgres-16 psql -U postgres -f /tmp/backup.sql
 	@echo "PostgreSQL 16 migrated successfully"
