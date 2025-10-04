bd62180589710833c076115d9e03c6f2ca15228a
Evanlab02 <evanlabuschagne70@gmail.com>
2025-10-04 11:00:29 +0200

    chore: Fix some issues in elastic config

--- misc/elastic.mk
+++ misc/elastic.mk
@@ -1,6 +1,6 @@
 .PHONY: elastic-create-kibana-user
 elastic-create-kibana-user:
-	docker compose -f apps/elastic/compose.yaml exec elasticsearch curl -X POST "http://localhost:9200/_security/user/kibana_system/_password" \
+	docker compose exec elasticsearch curl -X POST "http://localhost:9200/_security/user/kibana_system/_password" \
 		-H "Content-Type: application/json" \
 		-u "elastic:${ELASTIC_PASSWORD}" \
 		-d '{"password":"${KIBANA_PASSWORD}"}'
