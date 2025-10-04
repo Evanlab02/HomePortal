387b81b9c656953a00f93850068f8e74eefac866
Evanlab02 <evanlabuschagne70@gmail.com>
2025-10-04 10:27:53 +0200

    chore: Updated maintenance notifications

--- misc/maintenance.mk
+++ misc/maintenance.mk
@@ -3,11 +3,11 @@ back-to-normal:
 	@echo "Sending back to normal notification..."
 	@docker compose exec ntfy ntfy pub \
 		-u $(NTFY_USER):$(NTFY_PW) \
-		--title "Server is back to normal and there is no more planned maintenance" \
+		--title "Server is back to normal" \
 		--priority default \
 		--tags computer,dizzy_face,success \
-		--message "Uh... title?" \
-		https://ntfy.labuschagne.xyz/hp-all
+		--message "..." \
+		https://ntfy.labuschagne.xyz/maintenance
 	@echo "Sent back to normal notification"
 	@echo "Done"
 
@@ -16,12 +16,11 @@ reboot:
 	@echo "Sending reboot notification..."
 	@docker compose exec ntfy ntfy pub \
 		-u $(NTFY_USER):$(NTFY_PW) \
-		--title "🔄 Surprise! Time for another reboot!" \
-		--priority urgent \
-		--tags computer,dizzy_face,warning\
-		--click https://giphy.com/gifs/have-you-tried-turning-it-off-and-on-again-F7yLXA5fJ5sLC \
-		--message "The server has decided it needs a little nap 💤 Don't worry, it'll be back faster than you can say 'have you tried turning it off and on again?'" \
-		https://ntfy.labuschagne.xyz/hp-all
+		--title "Rebooting!" \
+		--priority high \
+		--tags computer,warning \
+		--message "The server is restarting in 1 minute..." \
+		https://ntfy.labuschagne.xyz/maintenance
 	@echo "Sent reboot notification"
 	@echo "Scheduling reboot"
 	@sudo shutdown -r +1
@@ -33,72 +32,37 @@ shutdown:
 	@echo "Sending shutdown notification..."
 	@docker compose exec ntfy ntfy pub \
 		-u $(NTFY_USER):$(NTFY_PW) \
-		--title "🛑 The Great Server Hibernation" \
-		--priority urgent \
-		--tags sleeping,power,warning\
-		--click https://www.youtube.com/watch?v=Gb2jGy76v0Y \
-		--message "The server is rage quitting! 😤 Time for its beauty sleep. Please don't panic - it's just having an existential crisis and needs some alone time 🧘‍♂️" \
-		https://ntfy.labuschagne.xyz/hp-all
+		--title "Shutting down!" \
+		--priority high \
+		--tags sleeping,power,warning \
+		--message "The server is shutting down in 1 minute..." \
+		https://ntfy.labuschagne.xyz/maintenance
 	@echo "Sent shutdown notification"
 	@echo "Scheduling shutdown"
 	@sudo shutdown +1
 	@echo "Shutdown scheduled"
 	@echo "Done"
 
-.PHONY: testing
-testing:
-	@echo "Sending testing notification..."
+.PHONY: unstable
+unstable:
+	@echo "Sending unstable notification..."
 	@docker compose exec ntfy ntfy pub \
 		-u $(NTFY_USER):$(NTFY_PW) \
-		--title "Mad Science in Progress!" \
-		--priority default \
-		--tags test_tube,microscope,warning\
-		--click https://giphy.com/gifs/mrw-post-vote-YYfEjWVqZ6NDG \
-		--message "Alert! I am poking the server with a stick again 🥸 Things might get weird... or explode... or both! Consider this your warning and maybe go touch some grass 🌱" \
-		https://ntfy.labuschagne.xyz/hp-all
-	@echo "Sent testing notification"
+		--title "The server is undergoing active work and testing." \
+		--priority high \
+		--tags warning \
+		--message "Server quality might be degraded during this period." \
+		https://ntfy.labuschagne.xyz/maintenance
+	@echo "Sent unstable notification"
 	@echo "Done"
 
-.PHONY: update
-update:
-	@echo "Sending update notification..."
+.PHONY: maintenance
+maintenance:
+	@echo "Sending maintenance notification..."
 	@docker compose exec ntfy ntfy pub \
 		-u $(NTFY_USER):$(NTFY_PW) \
-		--title "Package Therapy Session Starting!" \
-		--priority default \
-		--tags package,arrows_counterclockwise,sparkles\
-		--click https://xkcd.com/1197/ \
-		--message "Time for the server's monthly vitamin supplements! 💊 All those outdated packages are about to get a reality check. Don't worry, I promise this won't hurt... much" \
-		https://ntfy.labuschagne.xyz/hp-all
-	@echo "Sent update notification"
-	@echo "Updating..."
-	@sudo nala update
-	@echo "Update completed"
-	@echo "Done"
-
-.PHONY: upgrade
-upgrade:
-	@echo "Sending upgrade notification..."
-	@docker compose exec ntfy ntfy pub \
-		-u $(NTFY_USER):$(NTFY_PW) \
-		--title "Server Glow-Up Transformation!" \
-		--priority urgent \
-		--tags rocket,sparkles,hammer_and_wrench\
-		--click https://giphy.com/gifs/loading-computer-cat-JIX9t2j0ZTN9S \
-		--message "Breaking news: The server is getting a makeover! 💅 Time to upgrade from peasant packages to fancy new ones. This might take a while, so grab some coffee ☕" \
-		https://ntfy.labuschagne.xyz/hp-all
-	@echo "Sent upgrade notification"
-	@echo "Upgrading..."
-	@sudo nala upgrade
-	@echo "Upgrade completed"
-	@echo "Sending upgrade completion notification"
-	@docker compose exec ntfy ntfy pub \
-		-u $(NTFY_USER):$(NTFY_PW) \
-		--title "Mission Accomplished (Probably)!" \
-		--priority urgent \
-		--tags partying_face,tada,checkered_flag\
-		--click https://www.youtube.com/watch?v=LDU_Txk06tM \
-		--message "Ta-da! 🎭 The server has successfully survived another upgrade! It's now 0.001% more awesome and 99% more likely to work... hopefully" \
-		https://ntfy.labuschagne.xyz/hp-all
-	@echo "Sent upgrade completion notification"
-	@echo "Done"
+		--title "The server is undergoing maintenance." \
+		--priority high \
+		--tags maintenance,warning \
+		--message "The server is undergoing maintenance, will be back to full quality in a few minutes... (I hope)" \
+		https://ntfy.labuschagne.xyz/maintenance
