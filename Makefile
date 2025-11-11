012d6896382cad8a0bfd357034ab1fbe29e53298
Evanlab02 <evanlabuschagne70@gmail.com>
2025-11-11 17:42:15 +0200

    chore: Add homeportalv2 for early testing

--- Makefile
+++ Makefile
@@ -6,3 +6,7 @@ include misc/caddy.mk
 include misc/docker.mk
 include misc/elastic.mk
 include misc/maintenance.mk
+
+.PHONY: superuser
+superuser:
+	docker compose exec hp-2-admin python manage.py createsuperuser
--- apps/base/src/tasks/main.py
+++ apps/base/src/tasks/main.py
@@ -11,7 +11,7 @@ from shared.tasks.qbit import sync_gluetun_port
 logger = logging.getLogger(__name__)
 
 
-@app.on_after_configure.connect  # type: ignore
-def setup_periodic_tasks(sender: Celery, **_: Any) -> None:
-    """Periodic tasks setup."""
-    sender.add_periodic_task(600.0, sync_gluetun_port.s())  # type: ignore
+# @app.on_after_configure.connect  # type: ignore
+# def setup_periodic_tasks(sender: Celery, **_: Any) -> None:
+#     """Periodic tasks setup."""
+#     sender.add_periodic_task(600.0, sync_gluetun_port.s())  # type: ignore
